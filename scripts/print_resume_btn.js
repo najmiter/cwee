a3aee433d452848b7eedcc5050589a3d441610bd
Najam <85332859+najmiter@users.noreply.github.com>
2024-02-29 17:13:31 +0500

    make sure we get it right

--- scripts/print_resume_btn.js
+++ scripts/print_resume_btn.js
@@ -5,18 +5,9 @@ const cwee = {
     educations,
 };
 
-const elements = {
-    personal_details_elements: person_elements,
-    work_experiences_elements: job_elements,
-    skills_elements: skill_elements,
-    educations_elements: education_elements,
-};
-
 const print_resume_btn = document.getElementById("print-resume-btn");
 
 print_resume_btn.addEventListener("click", () => {
     localStorage.setItem("cwee", JSON.stringify(cwee));
-    localStorage.setItem("elements", JSON.stringify(elements));
     window.location.href = "./templates/t-0/main.html";
-    console.log("acha");
 });
--- templates/t-0/main.js
+++ templates/t-0/main.js
@@ -1,38 +1,44 @@
 const cwee = JSON.parse(localStorage.getItem("cwee"));
-const elements = JSON.parse(localStorage.getItem("elements"));
-const sections = Object.keys(cwee);
 
-// console.log(cwee["personal_details"]);
-// console.log(elements["personal_details_elements"]);
+if (cwee) {
+    const main = document.createElement("main");
+    main.setAttribute("class", "wrapper");
+    const section_div = document.createElement("hr");
+    section_div.setAttribute("class", "section-dividor");
 
-const main = document.createElement("main");
-main.setAttribute("class", "wrapper");
-const section_div = document.createElement("hr");
-section_div.setAttribute("class", "section-dividor");
+    const header = CreatePersonalDetails(cwee["personal_details"].at(0));
+    main.appendChild(header);
+    main.appendChild(document.createElement("hr"));
 
-const header = CreatePersonalDetails(cwee["personal_details"].at(0));
-main.appendChild(header);
-main.appendChild(document.createElement("hr"));
+    const work_section = CreateWorkExperience(cwee["work_experiences"]);
+    main.appendChild(work_section);
+    main.appendChild(section_div.cloneNode());
 
-const work_section = CreateWorkExperience(cwee["work_experiences"]);
-main.appendChild(work_section);
-main.appendChild(section_div.cloneNode());
+    const skill_section = CreateSkills(cwee["skills"]);
+    main.appendChild(skill_section);
+    main.appendChild(section_div.cloneNode());
 
-const skill_section = CreateSkills(cwee["skills"]);
-main.appendChild(skill_section);
-main.appendChild(section_div.cloneNode());
+    const education_section = CreateEducation(cwee["educations"]);
+    main.appendChild(education_section);
+    main.appendChild(section_div.cloneNode());
 
-const education_section = CreateEducation(cwee["educations"]);
-main.appendChild(education_section);
-main.appendChild(section_div.cloneNode());
+    document.getElementById("jism").appendChild(main);
 
-document.getElementById("jism").appendChild(main);
+    const progress_bar = document.querySelectorAll(".progress");
+    progress_bar.forEach((bar) => {
+        if (bar.getAttribute("value") < 50) {
+            bar.style.setProperty("--_clr-progrees", "var(--clr-dim)");
+        }
+    });
 
-const progress_bar = document.querySelectorAll(".progress");
-progress_bar.forEach((bar) => {
-    if (bar.getAttribute("value") < 50) {
-        bar.style.setProperty("--_clr-progrees", "var(--clr-dim)");
-    }
-});
+    print(document);
 
-print(document);
+    localStorage.clear();
+} else {
+    document
+        .getElementById("daddy")
+        .setAttribute(
+            "msg",
+            "Please go back and provide all the data before printing 😁"
+        );
+}
